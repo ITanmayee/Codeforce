72eab0fa017823bc6d4cef23a6e76e2b1db79424
ITanmayee <67579192+ITanmayee@users.noreply.github.com>
2021-04-28 21:27:56 +0530

    Compute the minimum number of 2050-numbers required to represent n as the sum of some (not necessarily distinct) 2050-numbers. .

---
+++ sum_of_2050.py
@@ -0,0 +1,33 @@
+"""
+
+A number is called 2050-number if it is 2050, 20500, ..., (2050⋅10k for integer k≥0).
+
+Given a number n, you are asked to represent n as the sum of some (not necessarily distinct) 2050-numbers. Compute the minimum number of 2050-numbers required for that.
+
+"""
+
+def can(num):
+    if num < 2050:
+        return - 1
+    
+    if num % 2050 != 0:
+        return -1
+    if len(str(num)) == 4:
+        return num // 2050
+    digit = len(str(num)) - 4
+    div = 2050 * (10 ** digit)
+    ans = []
+    while num >= 2050:
+        ans.append(num // div)
+        num = num - ((num // div) * div)
+        digit = len(str(num)) - 4
+        div = 2050 * (10 ** digit)
+        if num < div:
+            digit -= 1
+            div = 2050 * (10 ** digit)
+    return sum(ans)
+
+
+for _ in range (int(input())):
+    num = int(input())
+    print(can(num))
